d43fc86a0d37e03f129927457478503c818c75a3
ichankun15 <villafuerteitchan@gmail.com>
2025-09-18 19:21:05 +0800

    for gh-pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
+// import { Toaster } from "@/components/ui/toaster";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Index from "./pages/Index";
+// import NotFound from "./pages/NotFound";
+
+// const queryClient = new QueryClient();
+
+// const App = () => (
+//   <QueryClientProvider client={queryClient}>
+//     <TooltipProvider>
+//       <Toaster />
+//       <Sonner />
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/portfolio" element={<Index />} />
+//           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </TooltipProvider>
+//   </QueryClientProvider>
+// );
+
+// export default App;
+
+
+// only use this for deployment in gh-pages
+// GitHub Pages is static, so when you refresh or access a route like /about, it looks for /about/index.html on the server—but it doesn’t exist. 
+// That’s why you might get a 404 page.
+// -----
+// The fix is to use HashRouter instead of BrowserRouter. 
+// The hash (#) in the URL tells the browser to stay on the same HTML page and let React handle routing on the client side.
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // <- use HashRouter
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,15 +49,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
 
 export default App;
+
